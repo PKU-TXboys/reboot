0352909b7d78a8ce592d8940aa77a658ed6699f6
burning846 <893070935@qq.com>
2018-11-28 00:32:04 +0800

    basic activity & reserve

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -5,7 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    listData: [
+      { "code": "01", "text": "text1", "type": "type1" },
+      { "code": "02", "text": "text2", "type": "type2" },
+      { "code": "03", "text": "text3", "type": "type3" },
+      { "code": "04", "text": "text4", "type": "type4" },
+      { "code": "05", "text": "text5", "type": "type5" },
+      { "code": "06", "text": "text6", "type": "type6" },
+      { "code": "07", "text": "text7", "type": "type7" }
+    ]
   },
 
   /**
