04a6cac40469f8f8c6c6c56418de0ae06687e82c
burning846 <893070935@qq.com>
2018-11-29 01:40:44 +0800

    activity静态页面完善

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -5,15 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    listData: [
-      { "code": "01", "text": "text1", "type": "type1" },
-      { "code": "02", "text": "text2", "type": "type2" },
-      { "code": "03", "text": "text3", "type": "type3" },
-      { "code": "04", "text": "text4", "type": "type4" },
-      { "code": "05", "text": "text5", "type": "type5" },
-      { "code": "06", "text": "text6", "type": "type6" },
-      { "code": "07", "text": "text7", "type": "type7" }
-    ],
     activity_data: {
       "activity_id": 1,
       "title": "电脑小队第二次活动",
